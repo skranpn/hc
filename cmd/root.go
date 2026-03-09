58507d263659b03985b579ce737759d8abb4a483
skranpn <126283803+skranpn@users.noreply.github.com>
2026-03-09 21:35:34 +0900

    set run as default subcommand

--- cmd/root.go
+++ cmd/root.go
@@ -18,6 +18,23 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
+	defaultCommand := "run"
+	var cmdFound bool
+	cmd := rootCmd.Commands()
+
+	for _, a := range cmd {
+		for _, b := range os.Args[1:] {
+			if a.Name() == b {
+				cmdFound = true
+				break
+			}
+		}
+	}
+	if !cmdFound {
+		args := append([]string{defaultCommand}, os.Args[1:]...)
+		rootCmd.SetArgs(args)
+	}
+
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
